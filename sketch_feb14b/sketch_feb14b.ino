1c2f808025bc91c2ff7790577e8b479a1816f503
Gianpaolo Macario <gmacario@gmail.com>
2015-02-15 09:15:18 +0100

    Add code for blinking LED on digital out 13
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- sketch_feb14b/sketch_feb14b.ino
+++ sketch_feb14b/sketch_feb14b.ino
@@ -1,9 +1,24 @@
+/*
+  Turns on an LED for one seconds, then off for one second, repeat.
+
+  This example is adapted from Examples > 01.Basics > Blink
+
+ */
+
+// On the Arduino UNO the onboard LED is attached to digital pin 13
+#define LED 13
+
 void setup() {
   // put your setup code here, to run once:
 
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  digitalWrite(LED, HIGH);     // turn the LED on
+  delay(1000);
+  digitalWrite(LED, LOW);      // turn the LED off
+  delay(1000);
 }
